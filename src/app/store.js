39d5200a41a9fb59ca6e01e2b3d770f93a30117e
Denny Scott <dennyscott301@gmail.com>
2017-04-30 19:05:13 -0500

    Add thunk middle ware, connect to ghost blog

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,23 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers/index';
+
+const loggerMiddleware = createLogger();
 
 const defaultState = {};
 
 const enhancers = compose(
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  ),
   window.devToolsExtension ? window.devToolsExtension() : f => f
 );
 
-const store = createStore(rootReducer, defaultState, enhancers);
+const store = createStore(
+  rootReducer,
+  defaultState,
+  enhancers);
 
 export default store;
