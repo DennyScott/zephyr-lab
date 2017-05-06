a41955ea3cfcfe13f8dc3d61f43b466a1a2f81b6
Denny Scott <dennyscott301@gmail.com>
2017-05-06 18:27:58 -0500

    Add blog reducer

--- src/reducers/blog.js
+++ src/reducers/blog.js
@@ -1,5 +1,15 @@
+import { RECEIVE_BLOG } from '../actions/blog';
+
 const blog = (state = [], action) => {
-  return state;
+  switch(action.type) {
+    case RECEIVE_BLOG:
+      return {
+        ...state,
+        blog: action.data
+      }
+    default:
+      return state;
+  }
 }
 
 export default blog;
