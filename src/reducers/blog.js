e881f2bfe8d4240e23ac512cdf35223fb16e92d0
Denny Scott <dennyscott301@gmail.com>
2017-05-06 18:48:50 -0500

    test

--- src/reducers/blog.js
+++ src/reducers/blog.js
@@ -5,7 +5,7 @@ const blog = (state = [], action) => {
     case RECEIVE_BLOG:
       return {
         ...state,
-        blog: action.data
+        test: action.data
       }
     default:
       return state;
